0dcb2de351b578b79d01a877537fb905ff14f40b
Peter Szalai <szalaipeti.vagyok@gmail.com>
2018-08-12 13:05:01 +0200

    check empty stack in blanced-brackets.go

--- hackerrank/algorithms/balanced-brackets.go
+++ hackerrank/algorithms/balanced-brackets.go
@@ -15,23 +15,36 @@ func match(b1 rune, b2 rune) bool {
 }
 
 func isBalanced(s string) bool {
+	//println(s)
 	stack := []rune{}
 	for _, b := range s {
+		//println(b, stack)
 		if isOpening(b) {
 			stack = append(stack, b)
 			continue
 		}
-		if match(stack[len(stack)-1], b) {
+		if len(stack) != 0 && match(stack[len(stack)-1], b) {
 			stack = stack[0 : len(stack)-1]
 			continue
 		}
+		return false
+	}
+	if len(stack) != 0 {
 		return false
 	}
 	return true
 }
 
 func main() {
-	fmt.Println(isBalanced("{[()]}"))
-	fmt.Println(isBalanced("{[(])}"))
-	fmt.Println(isBalanced("{{[[(())]]}}"))
+	var T int
+	fmt.Scanf("%d", &T)
+	for i := 0; i < T; i++ {
+		var line string
+		fmt.Scanf("%s", &line)
+		if isBalanced(line) {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+	}
 }
