20339668c8fd460660fd62060fe5f8074281f1b9
Peter Szalai <szalaipeti.vagyok@gmail.com>
2018-08-15 21:15:20 +0200

    add circular queue implementation in go

---
+++ algorithms/circular-queue/circular-queue.go
@@ -0,0 +1,70 @@
+package queue
+
+type MyCircularQueue struct {
+	queue []int
+	head  int
+	size  int
+	cap   int
+}
+
+/** Initialize your data structure here. Set the size of the queue to be k. */
+func Constructor(k int) MyCircularQueue {
+	return MyCircularQueue{queue: make([]int, k), cap: k}
+}
+
+/** Insert an element into the circular queue. Return true if the operation is successful. */
+func (this *MyCircularQueue) EnQueue(value int) bool {
+	if this.size >= this.cap {
+		return false
+	}
+	this.queue[(this.head+this.size)%this.cap] = value
+	this.size += 1
+	return true
+}
+
+/** Delete an element from the circular queue. Return true if the operation is successful. */
+func (this *MyCircularQueue) DeQueue() bool {
+	if this.size <= 0 {
+		return false
+	}
+	this.head = (this.head + 1) % this.cap
+	this.size--
+	return true
+}
+
+/** Get the front item from the queue. */
+func (this *MyCircularQueue) Front() int {
+	if this.size <= 0 {
+		return -1
+	}
+	return this.queue[this.head]
+}
+
+/** Get the last item from the queue. */
+func (this *MyCircularQueue) Rear() int {
+	if this.size == 0 {
+		return -1
+	}
+	return this.queue[(this.head+this.size-1)%this.cap]
+}
+
+/** Checks whether the circular queue is empty or not. */
+func (this *MyCircularQueue) IsEmpty() bool {
+	return this.size == 0
+}
+
+/** Checks whether the circular queue is full or not. */
+func (this *MyCircularQueue) IsFull() bool {
+	return this.cap == this.size
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.EnQueue(value);
+ * param_2 := obj.DeQueue();
+ * param_3 := obj.Front();
+ * param_4 := obj.Rear();
+ * param_5 := obj.IsEmpty();
+ * param_6 := obj.IsFull();
+ */
---
+++ algorithms/circular-queue/circular-queue_test.go
@@ -0,0 +1,60 @@
+package queue
+
+import (
+	"log"
+	"testing"
+)
+
+func TestCircularQueue(t *testing.T) {
+	q := Constructor(3)
+	if !q.EnQueue(1) {
+		t.Error("there should be space for this operation")
+	}
+	log.Printf("after inserting 1: %v", q)
+	q.EnQueue(2)
+	log.Printf("after inserting 2: %v", q)
+	q.EnQueue(3)
+	log.Printf("after inserting 3: %v", q)
+
+	if q.EnQueue(4) {
+		t.Error("ther should not be capacity for this ")
+	}
+	r := q.Rear()
+	if r != 3 {
+		t.Errorf("got: %d, should be: %d", r, 3)
+	}
+	if !q.IsFull() {
+		t.Error("it should be full")
+	}
+	if !q.DeQueue() {
+		t.Error("could not dequeue!")
+	}
+	log.Printf("after dequeue: %v", q)
+
+	if !q.EnQueue(4) {
+		t.Error("could not enqueue")
+	}
+	r = q.Rear()
+	if r != 4 {
+		t.Errorf("got: %d, should be: %d", r, 4)
+	}
+}
+
+func TestComplex(t *testing.T) {
+	q := Constructor(81)
+	q.EnQueue(69)
+	log.Printf("Enqueue 69: %v", q)
+	q.DeQueue()
+	log.Printf("dequeue: %v", q)
+	q.EnQueue(92)
+	log.Printf("Enqueue 92: %v", q)
+	q.EnQueue(12)
+	log.Printf("Enqueue 12: %v", q)
+	q.DeQueue()
+	log.Printf("dequeue: %v", q)
+	f := q.Front()
+	if f != 12 {
+		t.Errorf("got: %d, expected: %d", f, 12)
+	}
+
+}
