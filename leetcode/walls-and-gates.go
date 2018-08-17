b5a33a0548a49ca015554b75645b302c2fc1b074
Peter Szalai <szalaipeti.vagyok@gmail.com>
2018-08-17 18:46:07 +0200

    add walls-and-gates solution

--- leetcode/walls-and-gates.go
+++ leetcode/walls-and-gates.go
@@ -1,6 +1,9 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 type NodePosition struct {
 	i, j int
@@ -9,11 +12,25 @@ type NodePosition struct {
 func bfs(rooms [][]int, i, j int) {
 	q := list.New()
 	q.PushBack(NodePosition{i, j})
+	N := len(rooms)
+	M := len(rooms[0])
+	directions := [][]int{
+		[]int{0, 1},
+		[]int{0, -1},
+		[]int{1, 0},
+		[]int{-1, 0},
+	}
 	for q.Len() > 0 {
-		for _, i := range []int{-1, 0, 1} {
-			for _, j := range []int{-1, 0, 1} {
-
+		next := q.Front().Value.(NodePosition)
+		q.Remove(q.Front())
+		for i := range directions {
+			x := directions[i][0] + next.i
+			y := directions[i][1] + next.j
+			if x < 0 || x >= N || y < 0 || y >= M || rooms[x][y] == -1 || rooms[x][y] <= rooms[next.i][next.j] {
+				continue
 			}
+			q.PushBack(NodePosition{x, y})
+			rooms[x][y] = rooms[next.i][next.j] + 1
 		}
 	}
 }
@@ -25,5 +42,19 @@ func wallsAndGates(rooms [][]int) {
 				bfs(rooms, i, j)
 			}
 		}
+	}
+}
+
+func main() {
+	inf := 1 << 31
+	rooms := [][]int{
+		[]int{inf, -1, 0, inf},
+		[]int{inf, inf, inf, -1},
+		[]int{inf, -1, inf, -1},
+		[]int{0, -1, inf, inf},
+	}
+	wallsAndGates(rooms)
+	for i := range rooms {
+		fmt.Println(rooms[i])
 	}
 }
