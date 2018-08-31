e372fe8cbc2d318369bac6807de61edeb6ca24e5
Peter Szalai <szalaipeti.vagyok@gmail.com>
2018-08-31 20:57:05 +0200

    add bribe - not solved yet

---
+++ hackerrank/algorithms/bribe.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func solver(arr []int) int {
+	var 
+}
+
+func main() {
+	fmt.Println(solver([]int{3, 4, 5, 6, 1, 2}))
+	fmt.Println(solver([]int{3, 4, 5, 6, 1, 2}))
+	fmt.Println(solver([]int{3, 4, 5, 6, 1, 2}))
+}
