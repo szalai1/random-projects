5062fd996de06b68d7b7b1113f742862a0a1ecc8
szalai1 <szalaipeti.vagyok@gmail.com>
2019-05-24 10:16:04 +0100

    add brilliant.org

---
+++ brilliant-org/dynamic-programming/denominations.py
@@ -0,0 +1,16 @@
+#! /bin/python3
+
+# count the number of subset of S where sum(S)=N
+def  count(S: list, N: int)-> int: 
+    if len(S) == 0:
+        return 0
+    count = N
+    for i in S:
+        subS = S
+        subS.remove(i)
+        count = f(subS, N-i)
+    return minNum
+
+for n in [1,2,3,4,5,6,7]:
+    print(n, count([1,5,10, 20 , 50])
+
