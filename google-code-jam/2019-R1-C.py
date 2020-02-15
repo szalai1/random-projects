6d17d3469a7a202794866e79d9e55dc3371f73b5
szalai1 <szalaipeti.vagyok@gmail.com>
2020-02-15 21:33:07 +0000

    19-r1-c solution

--- google-code-jam/2019-R1-C.py
+++ google-code-jam/2019-R1-C.py
@@ -14,7 +14,7 @@ def read_2_ints():
 def read_N_strs(N):
     strs = []
     for i in range(N):
-        strs.append(read_str())
+        strs.append(read_str()[::-1])
     return strs
 
 def read_str():
@@ -34,14 +34,50 @@ def print_ans(case_counter, ans):
 def print_ans_2(case_counter, ans1, ans2):
     print("Case #{case}: {ans1} {ans2}".format(case=case_counter+1, ans1=ans1, ans2=ans2))
 
+def solver(ws):
+    if len(ws) <= 1:
+        return 0, 1
+    if len(ws) == 2:
+        return 2, 0
+    new_w = []
+    n = 0
+    for w in ws:
+        if len(w) == 1:
+            n+=1
+            continue
+        new_w.append(w[1:])
+    n_ = 0
+    y_ = 0
+    used = False
+    for k, g in itertools.groupby(new_w, lambda w: w[0]):
+        x, y = solver(list(g))
+        n_ += x
+        y_ += y
+
+    if n + y_ >= 2:
+        n_ += 2
+        if n == 0:
+            y_ -= 2
+        if n == 1:
+            y_ -= 1
+            n -= 1
+        if n == 2:
+            n -= 2
+    return n_, n + y_
+
 def main():
     cases = read_int()
     for case in range(cases):
         N = read_int()
         words = read_N_strs(N)
-        words.sort(key=lambda w: w[::-1])
-        print words
-
-    print_ans(case, impossible)
+        words.sort()
+        n = 0
+        x_ = 0
+        y_ = 0
+        for k, g in itertools.groupby(words, lambda w: w[0]):
+            x, y  = solver(list(g))
+            x_  += x
+            y_ += y
+        print_ans(case, x_)
 
 main()
