3dacc1a2088ee013af6ffcdc2f0036a9116fc61b
szalai1 <szalaipeti.vagyok@gmail.com>
2021-12-01 09:28:23 +0100

    fix typo

--- advent-of-code/2021/day-1.clj
+++ advent-of-code/2021/day-1.clj
@@ -6,8 +6,8 @@
 (defn number-of-increases [nums]
   (reduce #(+ %1 (if (< (first %2) (second %2)) 1 0)) 0 (partition 2 1 nums)))
 
-(def first-start (number-of-increases nums))
-(def second-start (number-of-increases (map #(reduce + %) (partition 3 1 nums))))
+(def first-star (number-of-increases nums))
+(def second-star (number-of-increases (map #(reduce + %) (partition 3 1 nums))))
 
-(prn "#1: " first-start)
-(prn "#2: " second-start)
+(prn "#1: " first-star)
+(prn "#2: " second-star)
