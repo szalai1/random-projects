e5fdd65d3a991623b7e66edd84e5eb3b979abd38
szalai1 <szalaipeti.vagyok@gmail.com>
2021-12-01 09:26:58 +0100

    clean up

--- advent-of-code/2021/day-1.clj
+++ advent-of-code/2021/day-1.clj
@@ -1,12 +1,13 @@
-(pr "hello")
-
 (def nums (let [nums-str (with-open
                           [rdr (clojure.java.io/reader "day-1-input-1")]
                            (-> (line-seq rdr) vec))]
                 (map #(Integer/parseInt %) nums-str)))
 
-(defn first-start
+(defn number-of-increases [nums]
   (reduce #(+ %1 (if (< (first %2) (second %2)) 1 0)) 0 (partition 2 1 nums)))
 
-(defn second-start
-    (reduce #(+ %1 (if (< (first %2) (second %2)) 1 0)) 0 (partition 2 1 (map #(reduce + %)  (partition 3 1 nums)))))
+(def first-start (number-of-increases nums))
+(def second-start (number-of-increases (map #(reduce + %) (partition 3 1 nums))))
+
+(prn "#1: " first-start)
+(prn "#2: " second-start)
