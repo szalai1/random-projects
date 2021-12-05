59c6a2c28948fd15a2327d2ad28c2f207fc27cd9
Peter Szalai <szalaipeti.vagyok@gmail.com>
2021-12-05 12:42:50 +0100

    Create day-5.clj

---
+++ advent-of-code/2021/day-5.clj
@@ -0,0 +1,36 @@
+(require '[clojure.string :as st])
+
+(def input
+  (->>
+     (slurp "./resources/5.txt")
+     st/split-lines
+     (mapcat #(st/split % #"( -> |,)"))
+     (map #(Integer/parseInt %))
+     (partition 4 4)))
+
+(defn generate-line [x1 y1 x2 y2]
+  (let [coordinate-range (fn [[a b]]
+                           (let [direction (if (> a b) -1 1)]
+                             (range a (+ b direction) direction )))
+        x (coordinate-range [x1 x2])
+        y (coordinate-range [y1 y2])]
+    (if (or (= x1 x2) (= y1 y2))
+      (if (= 1 (count x))
+        (map #(vector (first x) %) y)
+        (map #(vector % (first y)) x))
+      (map vector x y))))
+
+(defn diagonal? [[x1 y1 x2 y2]]
+  (and (not (= x1 x2)) (not (= y1 y2))))
+
+(def non-diags (filter (complement diagonal?) input))
+
+(defn count-intersects [input]
+  (->> input
+       (mapcat #(apply generate-line %))
+       frequencies
+       (filter #(> (second %) 1))
+       count))
+
+(pr "#1: " (count-intersects non-diags))
+(pr "#2: " (count-intersects input))
