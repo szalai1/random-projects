5e7dd1e9dbf7ce48fbf9a92c1f56cb9523aee307
Peter Szalai <szalaipeti.vagyok@gmail.com>
2022-07-12 07:58:02 +0000

    finish bigger-is-greater

--- hackerrank/algorithms/bigger-is-greater.py
+++ hackerrank/algorithms/bigger-is-greater.py
@@ -6,16 +6,14 @@ def biggerIsGreater(w):
     for i in range(len(w)-1, 0, -1):
         a = w[i]
         b = w[i-1]
-        print(a, b)
         if a > b:
-            minmax = i-1
+            minmax = i
             for j in range(i, len(w)):
-                print(i, j, minmax)
                 if w[j] > w[i-1] and w[minmax] > w[j]:
                     minmax=j
-            print(minmax, i)
+            print(w[minmax], w[i-1])
             w[minmax], w[i-1] = w[i-1], w[minmax]
-            return "".join(list(w[0:i-1]) + list(sorted(w[i:])))
+            return "".join(list(w[0:i]) + list(sorted(w[i:])))
     return "no answer" 
 
 print(biggerIsGreater("dkhc"))
