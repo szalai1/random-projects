33351edd7aa85ffe8b8441d8c61961cd75f1dbec
szalai1 <szalaipeti.vagyok@gmail.com>
2023-12-01 22:56:36 +0100

    Automatic commit

--- advent-of-code/2023/src/day_1.clj
+++ advent-of-code/2023/src/day_1.clj
@@ -15,32 +15,14 @@
 
 ;; part 2
 (defn first-last-number-from-line-part-2 [line]
-  (let [digit-name-mapping {"one" "1"
-                            "two" "2"
-                            "three" "3"
-                            "four" "4"
-                            "five" "5"
-                            "six" "6"
-                            "seven" "7"
-                            "eight" "8"
-                            "nine" "9"
-                            "1" "1"
-                            "2" "2"
-                            "3" "3"
-                            "4" "4"
-                            "5" "5"
-                            "6" "6"
-                            "7" "7"
-                            "8" "8"
-                            "9" "9"}
+  (let [digit-name-mapping {"one" "1" "two" "2" "three" "3" "four" "4"
+                            "five" "5" "six" "6" "seven" "7" "eight" "8"
+                            "nine" "9" "1" "1" "2" "2" "3" "3" "4" "4"
+                            "5" "5" "6" "6" "7" "7" "8" "8" "9" "9"}
         rgx  (str "(?=(" (st/join "|" (keys digit-name-mapping)) "))")
-        _ (prn rgx)
-        digit-matching (Pattern/compile
-                        rgx)
-        matches (map digit-name-mapping (map second (re-seq digit-matching line)))
-        res (Integer/parseInt (str (first matches) (last matches)))]
-    (prn res line matches)
-    res))
+        digit-matching (Pattern/compile rgx)
+        matches (map digit-name-mapping (map second (re-seq digit-matching line)))]
+    (Integer/parseInt (str (first matches) (last matches)))))
 
 (->> (slurp "./resources/1.txt")
      (st/split-lines)
