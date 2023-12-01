b2da369ea473f57b94823832b78f1adf7fedc949
szalai1 <szalaipeti.vagyok@gmail.com>
2023-12-01 22:50:31 +0100

    Automatic commit

---
+++ advent-of-code/2023/src/day_1.clj
@@ -0,0 +1,48 @@
+(ns day-1  (:import (java.util.regex Pattern)))
+(require '[clojure.string :as st])
+
+;; part 1
+(defn first-last-number-from-line [line]
+  (let [f (re-find #"[0-9]" line)
+        l (re-find #"[0-9]" (st/reverse line))]
+    (Integer/parseInt (str f l))))
+
+(->>
+ (slurp "./resources/1.txt")
+ (st/split-lines)
+ (map first-last-number-from-line)
+ (reduce +))
+
+;; part 2
+(defn first-last-number-from-line-part-2 [line]
+  (let [digit-name-mapping {"one" "1"
+                            "two" "2"
+                            "three" "3"
+                            "four" "4"
+                            "five" "5"
+                            "six" "6"
+                            "seven" "7"
+                            "eight" "8"
+                            "nine" "9"
+                            "1" "1"
+                            "2" "2"
+                            "3" "3"
+                            "4" "4"
+                            "5" "5"
+                            "6" "6"
+                            "7" "7"
+                            "8" "8"
+                            "9" "9"}
+        rgx  (str "(?=(" (st/join "|" (keys digit-name-mapping)) "))")
+        _ (prn rgx)
+        digit-matching (Pattern/compile
+                        rgx)
+        matches (map digit-name-mapping (map second (re-seq digit-matching line)))
+        res (Integer/parseInt (str (first matches) (last matches)))]
+    (prn res line matches)
+    res))
+
+(->> (slurp "./resources/1.txt")
+     (st/split-lines)
+     (map first-last-number-from-line-part-2)
+     (reduce +))
