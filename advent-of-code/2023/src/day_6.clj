666be07d7de0d182e191923edcf696c4b591799c
szalai1 <szalaipeti.vagyok@gmail.com>
2023-12-06 12:41:11 +0100

    Automatic commit

---
+++ advent-of-code/2023/src/day_6.clj
@@ -0,0 +1,33 @@
+;; Time:        51     92     68     90
+;; Distance:   222   2031   1126   1225
+(def input-data [[51 222] [92 2031] [68 1126] [90 1225]])
+;;Time:      7  15   30
+;;Distance:  9  40  200
+(def input-data [[7 9] [15 40] [30 200]])
+(defn solve-second-order [a b c]
+  (let [d (- (* b b) (* 4 a c))
+        ] 
+    (if (< d 0)
+      nil
+      (let [x1 (/ (+ (- b) (Math/sqrt d)) (* 2 a))
+            x2 (/ (- (- b) (Math/sqrt d)) (* 2 a))]
+        (if (= x1 x2)
+          [x1]
+          [x1 x2])))))
+
+
+(defn solve-one [time distance]
+  (let [[x1 x2] (solve-second-order -1 time (- distance))
+        low (min x1 x2)
+        high (max x1 x2)
+        low-int (Math/ceil low )
+        high-int (Math/floor high)
+        offset (if (= low-int (Math/floor low)) -1 1)]
+    (int (+ offset
+            (- high-int 
+               low-int)))))
+;; part 1 
+(->> input-data 
+     (map #(apply solve-one %))
+     (reduce *))
+
